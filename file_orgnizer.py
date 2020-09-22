9bc4a07484d07f4aab893cd379a827db52210843
Ambuj Verma <31277384+ambujverma@users.noreply.github.com>
2020-09-22 15:05:21 +0530

    Add files via upload

---
+++ file_orgnizer.py
@@ -0,0 +1,27 @@
+import os
+import shutil
+
+path  = 'C:/Users/ILOVEYOU/Desktop/doc/'
+names = os.listdir(path)
+extension_set = set()
+
+for i in names:
+    ext = i.split(sep=".")
+    try:
+        extension_set.add(ext[1])
+    except:
+        continue
+
+for x in extension_set:
+    print(x)
+    if not os.path.exists(path+x):
+        os.mkdir(path+x)
+    
+for file in names:
+    ext = file.split(sep='.')
+    if len(ext)>1:
+        if ext[1] in file and not os.path.exists(path+ext[1]+'/'+file):
+            print(0)
+            shutil.move(path+file,path+ext[1]+'/'+file)
+ 
+    
